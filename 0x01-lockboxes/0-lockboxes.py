268ab3f55cabe94132894edea874144deb4fd61b
Muffinz1 <amiryou120@gmail.com>
2024-06-07 01:45:22 +0300

    solved

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+The module of the function that can whether unlock or not
+"""
+
+def canUnlockAll(boxes):
+    """
+    Defining a function that check if the boxes
+    can be unlocked or not
+    """
+    unlocked = [False] * len(boxes)
+    unlocked[0] = True
+    keys = [0]
+    while keys:
+        current_key = keys.pop()
+        for key in boxes[current_key]:
+            if key < len(boxes) and not unlocked[key]:
+                unlocked[key] = True
+                keys.append(key)
+    return all(unlocked)
---
+++ 0x01-lockboxes/README.md
@@ -0,0 +1,12 @@
+# Lockboxes
+## Concepts 
+- Lists and List Manipulation
+- Graph Theory Basics
+- Algorithmic Complexity
+- Recursion
+- Queue and Stack
+- Set Operations
+
+<a href="https://www.youtube.com/watch?feature=shared&v=V8DGdPkBBxg">Mock Interview</a>
+
+This directory includes a README file and a python file that includes the answer a main file to test.
