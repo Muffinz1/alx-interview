a1cd331e927bdc9c91151d72f3b3f0b51a675b5a
Muffinz1 <amiryou120@gmail.com>
2024-06-14 03:41:51 +0300

    number of operations

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+""" 
+A module that helps in
+calculating the minimum number of operations
+in order to achieve a given number of characters
+"""
+
+
+def minOperations(n: int) -> int:
+    """ 
+    Minimum Operations needed to get 
+    the number of H characters 
+    """
+    if n <= 1:
+        return 0
+    oper = 0
+    div = 2
+
+    while n > 1:
+        while n % div == 0:
+            oper += div
+            n //= div
+        div += 1
+    return oper
---
+++ 0x02-minimum_operations/README.md
@@ -0,0 +1,7 @@
+## 0x02. Minimum Operations
+# Resources
+* Code Optimization:
+* Prime Factorization
+* Dynamic Programming
+* Greedy Algorithms
+* Basic Python Programming
