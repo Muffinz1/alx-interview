d5091b24459ec814f44ee22c6f042da752c9cd4b
Muffinz <muffin@muffin.com>
2024-07-18 15:53:15 -0700

    rotated

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -3,6 +3,7 @@
 Rotating A 2D Matrix
 """
 
+
 def rotate_2d_matrix(matrix):
     """
     Defining a function to rotate the 2d matrix
@@ -11,7 +12,8 @@ def rotate_2d_matrix(matrix):
     for i in range(len(matrix)):
         for j in range(i, len(matrix)):
             matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
-    
+
     # reversing rows
     for row in matrix:
         row.reverse()
+
