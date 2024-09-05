fea27305fbe3de4fc13b49ee7ee26a82917e0291
Muffinz1 <amiryou120@gmail.com>
2024-09-05 22:59:57 +0300

    semi standard

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,45 +1,45 @@
 #!/usr/bin/node
 
-const request = require('request');
+const request = require('request')
 
 const fetchCharacter = (url, callback) => {
   request(url, (error, response, body) => {
     if (error) {
-      console.error(error);
-      return;
+      console.error(error)
+      return
     }
     try {
-      const character = JSON.parse(body).name;
-      console.log(character);
+      const character = JSON.parse(body).name
+      console.log(character)
     } catch (err) {
-      console.error('Error parsing JSON:', err);
+      console.error('Error parsing JSON:', err)
     }
-    callback();
-  });
-};
+    callback()
+  })
+}
 
 const fetchFilmCharacters = (filmId) => {
-  const filmUrl = `https://swapi-api.alx-tools.com/api/films/${filmId}`;
-  
+  const filmUrl = `https://swapi-api.alx-tools.com/api/films/${filmId}`
+
   request(filmUrl, (error, response, body) => {
     if (error) {
-      console.error(error);
-      return;
+      console.error(error)
+      return
     }
     try {
-      const characters = JSON.parse(body).characters;
-      
+      const characters = JSON.parse(body).characters
+
       const fetchNextCharacter = (index) => {
         if (index < characters.length) {
-          fetchCharacter(characters[index], () => fetchNextCharacter(index + 1));
+          fetchCharacter(characters[index], () => fetchNextCharacter(index + 1))
         }
-      };
-      
-      fetchNextCharacter(0);
-    } catch (err) {
-      console.error('Error parsing JSON:', err);
-    }
-  });
-};
+      }
 
-fetchFilmCharacters(process.argv[2]);
+      fetchNextCharacter(0)
+    } catch (err) {
+      console.error('Error parsing JSON:', err)
+    }
+  })
+}
+
+fetchFilmCharacters(process.argv[2])
