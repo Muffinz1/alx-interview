8b8c7a64840a95e9c687960a180fa1e1942ad361
Muffinz1 <amiryou120@gmail.com>
2024-09-17 10:58:41 +0300

    making change

---
+++ 0x08-making_change/0-making_change.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+""" defining a makechange function to deal with coins and change"""
+
+
+def makeChange(coins, total):
+    """
+    checks the change for everything,
+    sorts the coins backward in order to use the biggest coins first
+    determines the least number of coins required to meet a given
+    amount total.
+    """
+    if total <= 0:
+        return 0
+    if not coins:
+        return -1
+    coins.sort(reverse=True)
+    num_coins = 0
+    for coin in coins:
+        if coin <= total:
+            num_coins += total // coin
+            total %= coin
+            if total == 0:
+                return num_coins
+    return -1
---
+++ 0x08-making_change/README.md
@@ -0,0 +1,7 @@
+# Making change
+## Concepts :memo:
+* Greedy Algorithms
+* Dynamic Programming
+* Algorithmic Complexity
+* Problem-Solving Strategies
+* Python Programming
